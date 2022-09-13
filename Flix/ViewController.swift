7519643e88c02eb69777341a1d641b78565655b4
Eugene <75242911+EugenSong@users.noreply.github.com>
2022-09-13 13:45:08 -0700

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Eugene Song on 9/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
